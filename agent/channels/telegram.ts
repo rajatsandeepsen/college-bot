0922000120780d1fa2a04bf2f71e2122ab8097bc
Rajat Sandeep <93584596+rajatsandeepsen@users.noreply.github.com>
2026-09-11 00:28:56 +0530

    feat: onMessage callback

--- agent/channels/telegram.ts
+++ agent/channels/telegram.ts
@@ -1,9 +1,23 @@
-import { telegramChannel } from "eve/channels/telegram";
+import { defaultTelegramAuth, telegramChannel } from "eve/channels/telegram";
 
 export default telegramChannel({
 	botUsername: "sjcet_bot",
 	credentials: {
 		botToken: process.env.TELEGRAM_BOT_TOKEN,
 		webhookSecretToken: process.env.TELEGRAM_WEBHOOK_SECRET_TOKEN,
+	},
+	onMessage: (ctx, message) => {
+		console.log(ctx.telegram);
+		console.log("message.text", message.text);
+		console.log("chat.id", message.chat.id);
+
+		if (message.chat.type !== "private" || !message.from || message.from.isBot)
+			return null;
+
+		if (message.text === "subscribe") ctx.telegram.sendMessage("subscribed");
+
+		return {
+			auth: defaultTelegramAuth(message),
+		};
 	},
 });
